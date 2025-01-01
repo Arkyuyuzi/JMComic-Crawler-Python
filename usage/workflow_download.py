8f4a27bd2263a2c5f45c73d189a9bdbd7c119874
Arkyuyuzi <116791566+Arkyuyuzi@users.noreply.github.com>
2025-01-01 21:39:01 +0800

    Update workflow_download.py

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -1,6 +1,6 @@
 from jmcomic import *
 from jmcomic.cl import JmcomicUI
-
+JmModuleConfig.PFIELD_ADVICE['myname'] = lambda photo: f'【{photo.id}】{photo.title}'
 # 下方填入你要下载的本子的id，一行一个，每行的首尾可以有空白字符
 jm_albums = '''
 
